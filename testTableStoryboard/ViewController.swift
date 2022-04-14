2df0480acdf75e9921a7bc5185c39f147db5f6bc
Aleksandr Myskin <aleksandr.myskin@bsc-ideas.com>
2022-04-14 15:58:32 +0300

    Initial Commit

---
+++ testTableStoryboard/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  testTableStoryboard
+//
+//  Created by Alexander Myskin on 14.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
